b42b24270637910b824d6576913b5c1c009d5616
SItara Sheikh <sitarasheikh89@gmail.com>
2025-01-01 02:54:42 +0500

    update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: Ignoring ESLint errors can lead to code quality issues.
+    ignoreDuringBuilds: true,
+  },
 };
+
+module.exports = nextConfig;
